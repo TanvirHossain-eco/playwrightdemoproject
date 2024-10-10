a9202cea7e5188be3d14f6e81ded7e953b45e24c
TanvirHossain-eco <tanvir.echologyx@gmail.com>
2024-10-10 23:57:27 +0600

    Playwright Demo Project - 10/10/2024

--- tests/assertions.spec.js
+++ tests/assertions.spec.js
@@ -7,7 +7,8 @@ test.beforeAll(async () => {
     browser = await chromium.launch();
     const context = await browser.newContext();
     page = await context.newPage();
-    await page.goto('https://www.saucedemo.com/');
+    await page.goto('https://kitchen.applitools.com/');
+    await page.waitForTimeout(3000);
     // await loginpage(page);
     
   });
@@ -15,8 +16,8 @@ test.beforeAll(async () => {
 test.afterAll(async () => {
     await browser.close();
 });
-test('Assertions Demo', async () => {
-    await page.goto('https://kitchen.applitools.com');
+test('Assertions Demo 1', async () => {
+    // await page.goto('https://kitchen.applitools.com/');
     // await page.waitForTimeout(3000);
     // await page.pause();
     // Assertions
@@ -30,7 +31,60 @@ test('Assertions Demo', async () => {
         console.log('Element is unique');
         await page.locator('text=The Kitchen').click();
     }
-
-
-
+});
+// Check element hidden or visible
+test('Assertions Demo 2', async () => {
+    const visible = await page.locator('text=The Kitchen').isVisible();
+    if (visible) {
+        console.log('Element is visible');
+        await expect(await page.locator('text=The Kitchen')).toBeVisible();
+        await page.locator('text=The Kitchen').click();
+    }else {
+        console.log('Element is hidden');
+        await expect(await page.locator('text=The Kitchen')).toBeHidden();
+    }
+    // await expect(await page.locator('text=The Kitchen')).toBeVisible();
+    // await expect(await page.locator('text=The Kitchen')).toBeHidden();
+});
+// Check element enabled or disabled
+test('Assertions Demo 3', async () => {
+    const enabled = await page.locator('text=The Kitchen').isEnabled();
+    if (enabled) {
+        console.log('Element is enabled');
+        await expect(await page.locator('text=The Kitchen')).toBeEnabled();
+        await page.locator('text=The Kitchen').click();
+    }else {
+        console.log('Element is disabled');
+        await expect(await page.locator('text=The Kitchen')).toBeDisabled();
+    }
+    // await expect(await page.locator('text=The Kitchen')).toBeEnabled();
+    // await expect(await page.locator('text=The Kitchen')).toBeDisabled();
+});
+// Text Matches Value or Not
+test('Assertions Demo 4', async () => {
+    const text = await page.locator('text=The Kitchen').innerText();
+    if (text === 'The Kitchen') {
+        console.log('Text matches');
+        await expect(await page.locator('text=The Kitchen')).toHaveText('The Kitchen');
+        await page.locator('text=The Kitchen').click();
+    }else {
+        console.log('Text does not match');
+        await expect(await page.locator('text=The Kitchen')).not.toHaveText('The Kitchen');
+    }
+    // await expect(await page.locator('text=The Kitchen')).toHaveText('The Kitchen');
+    // await expect(await page.locator('text=The Kitchen')).not.toHaveText('The Kitchen');
+});
+// Element Attributes toHave & toHaveClass
+test('Assertions Demo 5', async () => {
+    const attr = await page.locator('text=The Kitchen').getAttribute('class');
+    if (attr === 'chakra-heading css-dpmy2a') {
+        console.log('Text matches');
+        await expect(await page.locator('text=The Kitchen')).toHaveClass('chakra-heading css-dpmy2a');
+        await page.locator('text=The Kitchen').click();
+    }else {
+        console.log('Text does not match');
+        await expect(await page.locator('text=The Kitchen')).not.toHaveClass('chakra-heading css-dpmy2a');
+    }
+    // await expect(await page.locator('text=The Kitchen')).toHaveClass('the-kitchen');
+    // await expect(await page.locator('text=The Kitchen')).not.toHaveClass('the-kitchen');
 });
